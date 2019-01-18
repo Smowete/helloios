c1371cd8f89e0e4553f0fb3e09fb4dfec1e417fa
Hongxiao Lyu <lyuh@uw.edu>
2019-01-18 11:37:36 -0800

    start

---
+++ helloios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  helloios
+//
+//  Created by Hongxiao Lyu on 1/18/19.
+//  Copyright © 2019 Hongxiao Lyu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
